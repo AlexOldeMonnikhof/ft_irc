249cdf685c3fc58874c04a0ff4d136c0d47bd33c
Alex Olde Monnikhof <a.oldemonnikhof@gmail.com>
2024-10-04 14:10:51 +0100

    improved cmd class

--- src/Command.cpp
+++ src/Command.cpp
@@ -7,53 +7,43 @@ Command::Command(string str)
 
 void  Command::parseCmd(string str)
 {
-    std::string ret;
-	bool inWord = false, column = false;
+    std::string word = "";
+    bool colonFound = false;
 
-	for (size_t i = 0; i < str.length(); i++)
-	{
-		if (i == str.length() - 1)
-		{
-			ret += '\0';
-			break;
-		}
-		if (isspace(str[i]) && inWord == false)
-			continue;
-		else if (str[i] == ':')
-		{
-			column = true;
-			continue;
-		}
-		else if (isspace(str[i]) && column == true)
-		{
-			ret += str[i];
-			continue;
-		}
-		else if (isspace(str[i]))
-			inWord = false;
-		else
-			inWord = true;
-		ret += str[i];
-	}
-	char *token = strtok((char *)ret.c_str(), " ");
-	while (token)
-	{
-		cmd.push_back(token);
-		token = strtok(nullptr, " ");
-	}
-	// cmd.push_back(nullptr);
-	for (size_t i = 0; i < cmd.size(); i++)
-	{
-		cout << "cmd " << i << " = " << cmd[i] << endl;
-	}
+    for (size_t i = 0; i < str.size(); ++i)
+    {
+        if (colonFound)
+            word += str[i];
+        else if (std::isspace(str[i]))
+        {
+            if (!word.empty())
+            {
+                cmd.push_back(word);
+                word.clear();
+            }
+        }
+        else if (str[i] == ':' && !colonFound)
+        {
+                colonFound = true;
+                cmd.push_back(word);
+                word.clear();
+        }
+        else
+            word += str[i];
+    }
+    word.erase(0, word.find_first_not_of(WHITE_SPACE));
+    if (!word.empty())
+        cmd.push_back(word);
+    // for (size_t i = 0; i < cmd.size(); i++)
+        // cout << "cmd " << i << " = " << cmd[i] << endl;
 }
 
-string	Command::getCmd(int i) const
+string  Command::getCmd(int i) const
 {
-	return cmd[i];
+    return cmd[i];
 }
 
-size_t	Command::getSize() const
+size_t  Command::getSize() const
 {
-	return cmd.size();
+    return cmd.size();
 }
