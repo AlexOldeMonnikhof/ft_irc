57d90b9cab9bed0e399024d6fce35ac2aec0f5f8
Luuk Van Geffen <lvan-gef@f0r5s1.codam.nl>
2025-01-31 19:36:23 +0100

    Format code

--- src/Channel.cpp
+++ src/Channel.cpp
@@ -7,31 +7,35 @@ Channel::Channel(int fd, std::string nick, std::string channelName)
     _inviteOnly = false;
     _topicOnlyOperators = false;
     _topic = "";
-    std::cout << nick << " (" << fd <<") created channel " << channelName << std::endl;
+    std::cout << nick << " (" << fd <<") created channel " << channelName << '\n';
     setName(channelName);
 }
 
 void    Channel::join(int fd, std::string nick)
 {
     if (_clients.find(nick) != _clients.end())
+    {
         sendMsg(fd, "Already in channel\n\r");
-    else
+    } else
     {
         _clients[nick] = false;
-        std::cout << nick << " joined channel " << _name << std::endl;
+        std::cout << nick << " joined channel " << _name << '\n';
     }
 }
 
 void    Channel::part(int fd, std::string nick)
 {
     _clients.erase(nick);
-    std::cout << nick << " (" << fd <<") left channel " << _name << std::endl;
+    std::cout << nick << " (" << fd <<") left channel " << _name << '\n';
 }
 
 bool    Channel::clientInChannel(std::string nick)
 {
     if (_clients.find(nick) != _clients.end())
+    {
         return true;
+    }
+
     return false;
 }
 
@@ -65,7 +69,10 @@ std::vector<std::string> Channel::getClients()
     std::vector<std::string>  clients;
 
     for (std::map<std::string, bool>::iterator iter = _clients.begin(); iter != _clients.end(); iter++)
+    {
         clients.push_back(iter->first);
+    }
+
     return clients;
 }
 
@@ -112,7 +119,9 @@ void    Channel::setTopic(std::string topic)
 void    Channel::printClients()
 {
     for (std::map<std::string, bool>::iterator iter = _clients.begin(); iter != _clients.end(); iter++)
+    {
         std::cout << iter->first << std::endl;
+    }
 }
 
 void    Channel::inviteUser(std::string nick)
@@ -133,7 +142,10 @@ void    Channel::removeOperator(std::string nick)
 bool    Channel::isFull() const
 {
     if (_userlimit && _clients.size() >= (size_t)_userlimit)
+    {
         return true;
+    }
+
     return false;
 }
 
@@ -142,8 +154,11 @@ bool    Channel::isUserInvited(std::string nick) const
     for (size_t i = 0; i < _inviteList.size(); i++)
     {
         if (_inviteList[i] == nick)
+        {
             return true;
+        }
     }
+
     return false;
 }
 
@@ -151,6 +166,9 @@ bool    Channel::isOperator(const std::string nick)
 {
     std::map<std::string, bool>::const_iterator it = _clients.find(nick);
     if (it != _clients.end())
+    {
         return it->second;
+    }
+
     return false;
 }
