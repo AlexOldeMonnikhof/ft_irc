b6e32d4833567b7486c433c2e711bc3a7663faaf
Luuk Van Geffen <lvan-gef@f0r5s1.codam.nl>
2025-01-31 19:49:06 +0100

    Format code

--- src/cmd/op.cpp
+++ src/cmd/op.cpp
@@ -4,22 +4,31 @@
 void    Server::modeOperator(int fd, Channel& channel, bool adding, Command& cmd)
 {
     if (cmd.getSize() < 4)
+    {
         sendMsg(fd, ERR_NEEDMOREPARAMS(_clients[fd].getNickname(), _host));
+    }
+
     std::string target = cmd.getCmd(3);
     if (!channel.isOperator(_clients[fd].getNickname()))
     {
         sendMsg(fd, ERR_CHANOPRIVSNEEDED(_host, _clients[fd].getNickname(), channel.getName()));
         return;
     }
+
     if (!channel.clientInChannel(target))
     {
         sendMsg(fd, ERR_USERNOTINCHANNEL(_host, channel.getName()));
         return;
     }
+
     if (adding)
+    {
         channel.setOperator(target);
-    else
+    } else
+    {
         channel.removeOperator(target);
+    }
+
     std::string mode = (adding ? "+o" : "-o");
     msgChannel(fd, RPL_MODEISOP(channel.getName(), _host, mode, target), channel.getName(), true);
 }
