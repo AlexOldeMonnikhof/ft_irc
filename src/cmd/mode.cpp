68d1894d12286b92a761f888c77a2bef977952cd
Luuk Van Geffen <lvan-gef@f0r5s1.codam.nl>
2025-01-31 19:47:56 +0100

    Format code

--- src/cmd/mode.cpp
+++ src/cmd/mode.cpp
@@ -8,46 +8,59 @@ void Server::cmdMode(int fd, Command& cmd)
         sendMsg(fd, ERR_NEEDMOREPARAMS(_clients[fd].getNickname(), _host));
         return;
     }
+
     std::string channel = cmd.getCmd(1);
     if (!channelExist(channel))
     {
         sendMsg(fd, ERR_NOSUCHCHANNEL(_host, channel, _clients[fd].getNickname()));
         return;
     }
+
     size_t i = getChannelIndex(_channels, channel);
 	if (!_channels[i].clientInChannel(_clients[fd].getNickname()))
 	{
 		sendMsg(fd, ERR_NOTONCHANNEL(_host, _channels[i].getName(), _clients[fd].getNickname()));
 		return;
 	}
+
     if (!_channels[i].isOperator(_clients[fd].getNickname()))
     {
         sendMsg(fd, ERR_CHANOPRIVSNEEDED(_host, _clients[fd].getNickname(), channel));
         return;
     }
+
     bool adding = false;
     std::string mode = cmd.getCmd(2);
     if (mode[0] == '+')
-        adding = true;
-    else if (mode[0] == '-')
-        adding = false;
-    else
     {
-        std::cout << "invalid mode: " << mode << std::endl;
+        adding = true;
+    } else if (mode[0] == '-')
+    {
+        adding = false;
+    } else
+    {
+        std::cout << "invalid mode: " << mode << '\n';
         return ;
     }
+
     size_t j = 0;
     while (mode[++j])
     {
         if (mode[j] == 'i')
+        {
             modeInviteOnly(fd, _channels[i], adding);
-        else if (mode[j] == 'o')
+        } else if (mode[j] == 'o')
+        {
             modeOperator(fd, _channels[i], adding, cmd);
-        else if (mode[j] == 'k')
+        } else if (mode[j] == 'k')
+        {
             modeKey(fd, _channels[i], adding, cmd);
-        else if (mode[j] == 't')
+        } else if (mode[j] == 't')
+        {
             modeTopic(fd, _channels[i], adding);
-        else if (mode[j] == 'l')
+        } else if (mode[j] == 'l')
+        {
             modeUserLimit(fd, _channels[i], adding, cmd);
+        }
     }
 }
