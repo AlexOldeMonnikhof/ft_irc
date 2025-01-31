2aa8f9bac428b701ec723cd95dc1abb23998bed8
Luuk Van Geffen <lvan-gef@f0r5s1.codam.nl>
2025-01-31 19:48:00 +0100

    Format code

--- src/cmd/nick.cpp
+++ src/cmd/nick.cpp
@@ -7,16 +7,19 @@ void    Server::cmdNICK(int fd, Command& cmd)
         sendMsg(fd, ERR_NONICKNAMEGIVEN(_clients[fd].getNickname(), _host));
         return;
     }
+
     if (!isValidName(cmd.getCmd(1)))
     {
         sendMsg(fd, ERR_ERRONEUSNICKNAME(cmd.getCmd(1), _host));
         return;
     }
+
     if (nickInUse(fd, cmd.getCmd(1)))
     {
         sendMsg(fd, ERR_NICKNAMEINUSE(cmd.getCmd(1), _host));
         return;
     }
+
     _clients[fd].setNickname(cmd.getCmd(1));
     _clients[fd].setRegister(NICKNAME);
 }
