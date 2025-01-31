eeecef57ce2d5a139405b7c61ea89942fde7e898
Luuk Van Geffen <lvan-gef@f0r5s1.codam.nl>
2025-01-31 19:51:05 +0100

    Format code

--- src/cmd/pass.cpp
+++ src/cmd/pass.cpp
@@ -7,14 +7,17 @@ void    Server::cmdPASS(int fd, Command& cmd)
         sendMsg(fd, ERR_NOTREGISTERED(_clients[fd].getNickname(), _host));
         return;
     }
+
     if (cmd.getSize() < 2)
     {
         sendMsg(fd, ERR_NEEDMOREPARAMS(_clients[fd].getNickname(), _host));
         return;
     }
+
     if (cmd.getCmd(1) == _password && cmd.getSize() == 2)
+    {
         _clients[fd].setRegister(PASSWORD);
-    else
+    } else
     {
         sendMsg(fd, ERR_PASSWDMISMATCH(_clients[fd].getNickname(), _host));
         return;
