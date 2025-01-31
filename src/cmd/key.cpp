f6a6364e76cdfd6a77c664f0ae375b1f2b2fab6b
Luuk Van Geffen <lvan-gef@f0r5s1.codam.nl>
2025-01-31 19:44:28 +0100

    Format code

--- src/cmd/key.cpp
+++ src/cmd/key.cpp
@@ -6,14 +6,14 @@ void    Server::modeKey(int fd, Channel& channel, bool adding, Command& cmd)
     if (adding)
     {
         if (cmd.getSize() < 4)
+        {
             sendMsg(fd, ERR_NEEDMOREPARAMS(_clients[fd].getNickname(), _host));
-        else
+        } else
         {
             channel.setPassword(cmd.getCmd(3));
             msgChannel(fd, RPL_MODEIS(channel.getName(), _host, "+k " + cmd.getCmd(3)), channel.getName(), true);
         }
-    }
-    else
+    } else
     {
         channel.setPassword("");
         msgChannel(fd, RPL_MODEIS(channel.getName(), _host, "-k"), channel.getName(), true);
