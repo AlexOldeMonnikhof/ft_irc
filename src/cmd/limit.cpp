fb6d19ec70e1a4cbe758edbe54defda201072e87
Luuk Van Geffen <lvan-gef@f0r5s1.codam.nl>
2025-01-31 19:46:07 +0100

    Format code

--- src/cmd/limit.cpp
+++ src/cmd/limit.cpp
@@ -7,23 +7,22 @@ void    Server::modeUserLimit(int fd, Channel& channel, bool adding, Command& cm
     if (adding)
     {
         if (cmd.getSize() < 4)
+        {
             sendMsg(fd, ERR_NEEDMOREPARAMS(_clients[fd].getNickname(), _host));
-        else
+        } else
         {
             limit = atoi(cmd.getCmd(3).c_str());
             if (limit && (limit > 0 && limit < 1000))
             {
                 channel.setUserLimit(limit);
                 msgChannel(fd, RPL_MODEISLIMIT(channel.getName(), _host, "+l", cmd.getCmd(3)), channel.getName(), true);
-            }
-            else
+            } else
             {
                 channel.setUserLimit(0);
                 msgChannel(fd, RPL_MODEISLIMIT(channel.getName(), _host, "-l", "0"), channel.getName(), true);
             }
         }
-    }
-    else
+    } else
     {
         channel.setUserLimit(0);
         msgChannel(fd, RPL_MODEISLIMIT(channel.getName(), _host, "-l", "0"), channel.getName(), true);
