019d3d820f0e2c83763af5e819d2949bc3d04d37
Luuk Van Geffen <lvan-gef@f0r5s1.codam.nl>
2025-01-31 19:54:31 +0100

    Format code

--- src/cmd/user.cpp
+++ src/cmd/user.cpp
@@ -7,11 +7,14 @@ void    Server::cmdUSER(int fd, Command& cmd)
         sendMsg(fd, ERR_NEEDMOREPARAMS(_clients[fd].getNickname(), _host));
         return ;
     }
+
     _clients[fd].setUsername(cmd.getCmd(1));
     _clients[fd].setHostname(cmd.getCmd(2));
     _clients[fd].setServername(cmd.getCmd(3));
     _clients[fd].setRealname(cmd.getCmd(4));
     _clients[fd].setRegister(USERNAME);
     if (_clients[fd].getRegister() == 7)
-        std::cout << "Client is now registered" << std::endl;
+    {
+        std::cout << "Client is now registered" << '\n';
+    }
 }
