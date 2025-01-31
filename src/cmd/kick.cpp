4362f772be73411a9a55f0c6f9ca279ea24b22f8
Luuk Van Geffen <lvan-gef@f0r5s1.codam.nl>
2025-01-31 19:45:09 +0100

    Format code

--- src/cmd/kick.cpp
+++ src/cmd/kick.cpp
@@ -8,6 +8,7 @@ void    Server::cmdKick(int fd, Command& cmd)
 		sendMsg(fd, ERR_NEEDMOREPARAMS(_clients[fd].getNickname(), _host));
 		return;
 	}
+
 	size_t i = getChannelIndex(_channels, cmd.getCmd(1));
 	std::string target = cmd.getCmd(2);
 	if (!channelExist(cmd.getCmd(1)))
@@ -15,17 +16,20 @@ void    Server::cmdKick(int fd, Command& cmd)
 		sendMsg(fd, ERR_NOSUCHCHANNEL(_host, cmd.getCmd(2), _clients[fd].getNickname()));
 		return;
 	}
+
     Channel& channel = _channels[i];
 	if (!channel.clientInChannel(_clients[fd].getNickname()))
 	{
 		sendMsg(fd, ERR_NOTONCHANNEL(_host, channel.getName(), _clients[fd].getNickname()));
 		return;
 	}
+
 	if (!channel.isOperator(_clients[fd].getNickname()))
     {
         sendMsg(fd, ERR_CHANOPRIVSNEEDED(_host, _clients[fd].getNickname(), channel.getName()));
 		return;
 	}
+
 	std::vector<std::string> targets = splitVector(cmd.getCmd(2), ',');
 	for (size_t i = 0; i < targets.size(); i++)
 	{
@@ -34,11 +38,13 @@ void    Server::cmdKick(int fd, Command& cmd)
             sendMsg(fd, ERR_NOSUCHNICK(_host, targets[i]));
             continue;
 		}
+
 		if (!channel.clientInChannel(targets[i]))
         {
             sendMsg(fd, ERR_USERNOTINCHANNEL(_host, channel.getName()));
 			continue;
 		}
+
         msgChannel(fd, RPL_KICK(_clients[fd].getNickname(), _clients[fd].getUsername(), _host, channel.getName(), targets[i]), channel.getName(), true);
         channel.part(getClientFd(targets[i]), targets[i]);
 	}
