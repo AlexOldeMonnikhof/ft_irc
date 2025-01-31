8463f666301ca016d52ffacf8b9dec146bc44c20
Luuk Van Geffen <lvan-gef@f0r5s1.codam.nl>
2025-01-31 19:39:56 +0100

    Format code

--- src/Command.cpp
+++ src/Command.cpp
@@ -11,11 +11,16 @@ void  Command::parseCmd(std::string str)
     bool colonFound = false;
     _cmd.clear();
     if (str.find_first_not_of(WHITE_SPACE) == std::string::npos)
+    {
         return;
+    }
+
     for (size_t i = 0; i < str.size(); i++)
     {
         if (colonFound)
+        {
             word += str[i];
+        }
         else if (std::isspace(str[i]))
         {
             if (!word.empty())
@@ -23,20 +28,25 @@ void  Command::parseCmd(std::string str)
                 _cmd.push_back(word);
                 word.clear();
             }
-        }
-        else if (str[i] == ':' && !colonFound)
+        } else if (str[i] == ':' && !colonFound)
         {
                 colonFound = true;
                 if (!word.empty())
+                {
                     _cmd.push_back(word);
+                }
                 word.clear();
-        }
-        else
+        } else
+        {
             word += str[i];
+        }
     }
+
     word.erase(0, word.find_first_not_of(WHITE_SPACE));
     if (!word.empty())
+    {
         _cmd.push_back(word);
+    }
 }
 
 std::vector<std::string>  Command::getV() const
