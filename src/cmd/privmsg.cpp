83a54d68d782f2a3eaaa6d2f08b12786ecb59211
Luuk Van Geffen <lvan-gef@f0r5s1.codam.nl>
2025-01-31 19:52:40 +0100

    Format code

--- src/cmd/privmsg.cpp
+++ src/cmd/privmsg.cpp
@@ -8,7 +8,10 @@ void    Server::msgChannel(int fd, std::string msg, std::string channel, bool se
     for (size_t i = 0; i < clientSize; i++)
     {
         if (!sendToSender && getClientFd(clients[i]) == fd)
+        {
             continue;
+        }
+
         std::string nick = _clients[fd].getNickname();
         sendMsg(getClientFd(clients[i]), msg);
     }
@@ -21,15 +24,20 @@ void    Server::privmsgChannel(int fd, Command& cmd, std::string channel)
         sendMsg(fd, ERR_NOSUCHCHANNEL(_host, channel, _clients[fd].getNickname()));
         return;
     }
+
     size_t index = getChannelIndex(_channels, channel);
     if (!_channels[index].clientInChannel(_clients[fd].getNickname()))
     {
         sendMsg(fd, ERR_NOTONCHANNEL(_host, channel, _clients[fd].getNickname()));
         return;
     }
+
     std::string op = "";
     if (_channels[index].isOperator(_clients[fd].getNickname()))
+    {
         op += "@";
+    }
+
     msgChannel(fd, PRIVMSG_FORMATCH(_clients[fd].getNickname() + op, _clients[fd].getUsername(), _host, channel, cmd.getCmd(2)), channel, false);
 }
 
@@ -37,9 +45,12 @@ void    Server::privmsgClient(int fd, Command& cmd, std::string nick)
 {
     int targetFd = getClientFd(nick);
     if (!targetFd)
+    {
         sendMsg(fd, ERR_NOSUCHNICK(_host, nick));
-    else
+    } else
+    {
         sendMsg(targetFd, PRIVMSG_FORMATUSER(_clients[fd].getNickname(), _clients[fd].getUsername(), _host, nick, cmd.getCmd(2)));
+    }
 }
 
 void    Server::cmdPrivmsg(int fd, Command& cmd)
@@ -50,12 +61,16 @@ void    Server::cmdPrivmsg(int fd, Command& cmd)
         sendMsg(fd, ERR_NEEDMOREPARAMS(_clients[fd].getNickname(), _host));
         return;
     }
+
     targets = splitVector(cmd.getCmd(1), ',');
     for (size_t i = 0; i < targets.size(); i++)
     {
         if (targets[i][0] == '#')
+        {
             privmsgChannel(fd, cmd, targets[i]);
-        else
+        } else
+        {
             privmsgClient(fd, cmd, targets[i]);
+        }
     }
 }
