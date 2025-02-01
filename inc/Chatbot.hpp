4ea30e42ed7dbecc349888a80b5fa1d9e3fc8694
luuk <lvan-gef@student.codam.nl>
2025-02-02 00:14:28 +0100

    Impl a simple chatbot

--- inc/Chatbot.hpp
+++ inc/Chatbot.hpp
@@ -2,14 +2,28 @@
 #define CHATBOT_HPP
 
 #include <string>
+#include <map>
+
 class Chatbot {
+    private:
+        std::string _nickname;
+        std::map<std::string, std::string> _commands;
+
+    private:
+        void initCommands();
+        std::string getCurrentTime();
+
     public:
-        Chatbot();
+        Chatbot(const std::string &nickname);
+
         Chatbot(const Chatbot &rhs);
         Chatbot &operator=(const Chatbot &rhs);
+
         ~Chatbot();
+
     public:
-        std::string getRespons();
+        const std::string &getNickname() const;
+        std::string getRespons(const std::string &sender, const std::string &message);
 };
 
 #endif // CHATBOT_HPP
--- src/Chatbot.cpp
+++ src/Chatbot.cpp
@@ -1,22 +1,71 @@
-#include "../inc/Chatbot.hpp"
+#include <cstdlib>
+#include <ctime>
+#include <string>
 
-Chatbot::Chatbot() {};
+#include "Chatbot.hpp"
 
-Chatbot::Chatbot(const Chatbot &rhs)
+Chatbot::Chatbot(const std::string &nickname) : _nickname(nickname)
 {
-    (void)rhs;
-}
+    initCommands();
+};
+
+Chatbot::Chatbot(const Chatbot &rhs) :
+    _nickname(rhs._nickname),
+    _commands(rhs._commands)
+{}
 
 Chatbot &Chatbot::operator=(const Chatbot &rhs)
 {
-    if (this != &rhs) {}
+    if (this != &rhs) {
+        _nickname = rhs._nickname;
+        _commands = rhs._commands;
+    }
 
     return *this;
 }
 
 Chatbot::~Chatbot() {}
 
-std::string Chatbot::getRespons()
+const std::string &Chatbot::getNickname() const
 {
-    return "Sorry i'm still learning so i can not help you on the moment";
+    return _nickname;
+}
+
+std::string Chatbot::getRespons(const std::string &sender, const std::string &message)
+{
+    if (sender == _nickname)
+    {
+        return "";
+    }
+
+    if (message.length() > 0 && message[0] == '!')
+    {
+        std::string command = message.substr(1);
+        std::map<std::string, std::string>::const_iterator it = _commands.find(command);
+        if (it != _commands.end())
+        {
+            return it->second;
+        }
+    }
+
+    if (message.find("hello") != std::string::npos || message.find("hi") != std::string::npos)
+    {
+        return "Hello " + sender + "! How can I help you today?";
+    }
+
+    return "Sorry i'm still in mine learning fase, and like my mentor i'm a slow learner, please have some patcience with me";
+}
+
+void Chatbot::initCommands()
+{
+    _commands["help"] = "Available commands: !help, !time, !about, !rules";
+    _commands["time"] = "Current server time: " + getCurrentTime();
+    _commands["about"] = "I'm a friendly IRC bot here to help!";
+    _commands["rules"] = "1. Be respectful 2. No spam 3. Have fun!";
+}
+
+std::string Chatbot::getCurrentTime()
+{
+    time_t now = time(0);
+    return std::string(ctime(&now));
 }
